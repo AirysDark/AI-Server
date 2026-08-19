242f173a3251eca19708ee08a182ccb8cec758db
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-08-20 01:05:23 +1000

    Add animated character display interface

--- esp32s3/include/display.h
+++ esp32s3/include/display.h
@@ -1,13 +1,17 @@
 #pragma once
 
 #include <Arduino.h>
+#include "animation.h"
 
 class AiDisplay {
 public:
     void begin();
+    void updateCharacter(AnimationState state, uint32_t frame = 0);
     void showBoot();
     void showWiFi(const String& status);
     void showServer(const String& status);
     void showMessage(const String& sender, const String& message);
     void showError(const String& message);
+private:
+    void drawCharacter(AnimationState state, uint32_t frame);
 };
