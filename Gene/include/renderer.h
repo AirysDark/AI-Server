62fb56c8b5f5049be695e06080d228f00a6fd4ce
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-08-20 04:30:48 +1000

    Fix Gene renderer double buffering header

--- Gene/include/renderer.h
+++ Gene/include/renderer.h
@@ -8,6 +8,7 @@ class Renderer {
 public:
     bool initialize(uint32_t width = 1280, uint32_t height = 720);
     void draw(const Model& model);
+    void paint();
     void setWindowTitle(const std::string& title);
     bool running() const;
     void pollEvents();
@@ -16,7 +17,12 @@ public:
 private:
     uint32_t _width = 1280;
     uint32_t _height = 720;
+    uint32_t _bufferWidth = 0;
+    uint32_t _bufferHeight = 0;
     bool _running = false;
     void* _window = nullptr;
+    void* _backBufferDC = nullptr;
+    void* _backBufferBitmap = nullptr;
+    void* _backBufferOldBitmap = nullptr;
 };
 }
